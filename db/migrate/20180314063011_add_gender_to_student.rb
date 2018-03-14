5637f5fe6bb9dbac4cef8736acbe0d741c9721ef
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-03-14 13:19:36 +0530

    before partial form

---
+++ db/migrate/20180314063011_add_gender_to_student.rb
@@ -0,0 +1,5 @@
+class AddGenderToStudent < ActiveRecord::Migration[5.1]
+  def change
+    add_column :students, :gender, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313041708) do
+ActiveRecord::Schema.define(version: 20180314063011) do
 
   create_table "students", force: :cascade do |t|
     t.string "firstname"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180313041708) do
     t.string "password_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "gender"
   end
 
 end
